633c4289dc55e38d1e5feec7a0fb79b5060c248e
amirnilsson <amirnilsson@gmail.com>
2021-11-16 20:02:27 +0100

    Calculator for tips and a bit of design

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var totalTip: UILabel!
+    
+    @IBOutlet weak var totalAmount: UITextField!
+    
+    @IBOutlet weak var procentage: UITextField!
+    
+    @IBOutlet weak var amountWithTip: UILabel!
+    
+    @IBAction func calcButton(_ sender: Any) {
+        
+        var tipAmount: Double = (Double(procentage.text!)! / 100) * Double(totalAmount.text!)!
+        
+        totalTip.text = String(format: "$%.00f", tipAmount)
+        
+        amountWithTip.text = String(format: "$%.00f",tipAmount + Double(totalAmount.text!)!)
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello")
+        
+        
+        }
+        
     }
 
 
-}
+
 
